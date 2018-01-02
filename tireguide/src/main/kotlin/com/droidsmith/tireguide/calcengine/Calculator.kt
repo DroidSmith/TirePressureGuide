d6b3560a7cae385702c342822d0a95135964dd25
DroidSmith <DroidSmith@users.noreply.github.com>
2018-01-02 01:44:55 -0600

    refactor: added parameters to the psi call instead of constructors. Returned a rounded value for psi. Replaced TextUtils with Objects.equals.

--- tireguide/src/main/kotlin/com/droidsmith/tireguide/calcengine/Calculator.kt
+++ tireguide/src/main/kotlin/com/droidsmith/tireguide/calcengine/Calculator.kt
@@ -1,51 +1,53 @@
 package com.droidsmith.tireguide.calcengine
 
-import android.text.TextUtils
+import java.util.*
 
 /**
  * Calculates the proper tire pressure for a bicycle tire based on weight and width of tire.
  */
-class Calculator(private val loadWeight: Double, private val tireWidth: String) {
+class Calculator {
 
     /**
-     * Uses magical numbers to determine the optimal tire pressure;
-     * @return The magical tire pressure;
+	 * Uses magical numbers to determine the optimal tire pressure.
+	 * @param loadWeight The weight in pounds exerted on the wheel.
+	 * @param tireWidth The width of the tire.
+	 * @return The magical tire pressure.
      */
-    fun psi(): Double {
+	fun psi(loadWeight: Double, tireWidth: String): Double {
         var m = 0.0
         var b = 0.0
         val psi: Double
 
-        if (TextUtils.equals("20", tireWidth)) {
+		if (Objects.equals(tireWidth, "20")) {
             m = 1.363636364
             b = -11.0
-        } else if (TextUtils.equals("21", tireWidth)) {
+		} else if (Objects.equals(tireWidth, "21")) {
             m = 1.181818182
             b = 0.0
-        } else if (TextUtils.equals("22", tireWidth)) {
+		} else if (Objects.equals(tireWidth, "22")) {
             m = 1.169601449
             b = -6.260144928
-        } else if (TextUtils.equals("23", tireWidth)) {
+		} else if (Objects.equals(tireWidth, "23")) {
             m = 1.136363636
             b = -9.5
-        } else if (TextUtils.equals("24", tireWidth)) {
+		} else if (Objects.equals(tireWidth, "24")) {
             m = 1.053507905
             b = -10.26594203
-        } else if (TextUtils.equals("25", tireWidth)) {
+		} else if (Objects.equals(tireWidth, "25")) {
             m = 1.0
             b = -13.0
-        } else if (TextUtils.equals("26", tireWidth)) {
+		} else if (Objects.equals(tireWidth, "26")) {
             m = 0.9522727273
             b = -12.55
-        } else if (TextUtils.equals("27", tireWidth)) {
+		} else if (Objects.equals(tireWidth, "27")) {
             m = 0.886363636
             b = -8.5
-        } else if (TextUtils.equals("28", tireWidth)) {
+		} else if (Objects.equals(tireWidth, "28")) {
             m = .8022580702
             b = -6.348534297
         }
 
         psi = loadWeight * m + b
-        return psi
+		return Math.round(psi).toDouble()
     }
 }
